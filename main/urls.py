a3758fab1e263757d70c8cfe9e6b27c22fdbec3c
alina <khomkolovaalina@gmail.com>
2025-04-26 19:09:14 +0200

    Сделала API запросы для моделей Product, Category, Subcategory

--- main/urls.py
+++ main/urls.py
@@ -1,17 +1,26 @@
 from django.urls import path
 
 from main.apps import MainConfig
-from main.views import BaseView, CategoryListView, ProductListView, ProductDetailView
+from main.views import SubcategoryListAPIView, SubcategoryRetrieveUpdateDestroyAPIView, SubcategoryCreatedAPIView, \
+    CategoryListAPIView, CategoryCreatedAPIView, CategoryRetrieveUpdateDestroyAPIView, ProductListAPIView
 
 app_name = MainConfig.name
 
 urlpatterns = [
-    # Главная страница
-    path('', BaseView.as_view(), name='home'),
-    # Категории и подкатегории товаров
-    path('categories/', CategoryListView.as_view(), name='categories'),
-    # Категории и подкатегории товаров
-    path('categories/<slug:slug>/', ProductListView.as_view(), name='product'),
-    # Описание товара
-    path('categories/<slug:slug>/product_detail/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    # Список категорий
+    path('api/category/', CategoryListAPIView.as_view(), name='category-list'),
+    # Создание категории
+    path('api/category/created/', CategoryCreatedAPIView.as_view(), name='category-created'),
+    # Удаление, изменение, просмотр одной позиции
+    path('api/category/<int:pk>/', CategoryRetrieveUpdateDestroyAPIView.as_view(), name='category-rud'),
+
+    # Список подкатегорий
+    path('api/subcategory/', SubcategoryListAPIView.as_view(), name='subcategory-list'),
+    # Создание подкатегории
+    path('api/subcategory/created/', SubcategoryCreatedAPIView.as_view(), name='subcategory-created'),
+    # Удаление, изменение, просмотр одной позиции товара
+    path('api/subcategory/<int:pk>/', SubcategoryRetrieveUpdateDestroyAPIView.as_view(), name='subcategory-rud'),
+
+    # Просмотр всех продуктов
+    path('api/product/', ProductListAPIView.as_view(), name='product-list'),
 ]
