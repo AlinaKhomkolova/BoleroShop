d2626ed40c713fe8a0d06d17f41264d5348b05b7
alina <khomkolovaalina@gmail.com>
2025-04-26 19:04:58 +0200

    Убрала абсолютный юрл

--- main/models.py
+++ main/models.py
@@ -24,9 +24,6 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-    def get_absolute_url(self):
-        return reverse('category_detail', args=[str(self.slug)])
-
 
 class Subcategory(models.Model):
     name = models.CharField(max_length=30,
