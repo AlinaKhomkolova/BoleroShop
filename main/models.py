337792af87ddc07b0d2ae32b31f6176dbe22e92e
alina <khomkolovaalina@gmail.com>
2025-04-26 19:02:35 +0200

    Убрала абсолютный юрл

--- main/models.py
+++ main/models.py
@@ -97,10 +97,6 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
-    def get_absolute_url(self):
-        return reverse('main:product_detail',
-                       args=[self.subcategories.slug])
-
     @property
     def sell_price(self):
         if self.discount:
